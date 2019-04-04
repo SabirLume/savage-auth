459c4eb84fea0491a50191ca8e841e02149fcc55
sabirlume <correct-sabirlume@gmail.com>
2019-04-04 10:53:50 -0400

    updated DB link to make the delete button work

--- public/main.js
+++ public/main.js
@@ -26,20 +26,21 @@ Array.from(thumbUp).forEach(function(element) {
 });
 
 Array.from(trash).forEach(function(element) {
-      element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
-        const msg = this.parentNode.parentNode.childNodes[3].innerText
-        fetch('messages', {
-          method: 'delete',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            'name': name,
-            'msg': msg
-          })
-        }).then(function (response) {
-          window.location.reload()
-        })
-      });
+  element.addEventListener('click', function(){
+    const name = this.parentNode.parentNode.childNodes[1].innerText;
+    const msg = this.parentNode.parentNode.childNodes[3].innerText;
+    fetch('messages', {
+      method: 'delete',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        'name': name,
+        'msg': msg
+      })
+    }).then(function (response) {
+      window.location.reload()
+    })
+  });
 });
+
