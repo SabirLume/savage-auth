36f40f6ee92314a70e52e82be8001bd34c972ab9
SabirLume <sabirlume@gmail.com>
2019-04-05 23:40:47 -0400

    database change

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb+srv://sabir:sabir@crud-1-x5qne.mongodb.net/test?retryWrites=true', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb+srv://sabir:sabir@crud-1-x5qne.mongodb.net/tes?retryWrites=true', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
     'dbName': 'notesapp'
 };
